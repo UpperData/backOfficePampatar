9126e666c53cfc0241cae18ca5c8183479ff0ee9
root <root@upperdata-server.cantv.net>
2020-09-30 22:47:35 -0400

    start

---
+++ package/starterkit/src/routes/Router.js
@@ -0,0 +1,19 @@
+import { lazy } from "react";
+
+const Dashboard = lazy(() => import("../views/dashboard/Dashboard"));
+
+var ThemeRoutes = [
+  {
+    navlabel: true,
+    name: "Personal",
+    icon: "mdi mdi-dots-horizontal",
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "ti-dashboard",
+    component: Dashboard,
+  },
+  { path: "/", pathTo: "/dashboard", name: "Dashboard", redirect: true },
+];
+export default ThemeRoutes;
---
+++ package/starterkit/src/routes/index.js
@@ -0,0 +1,6 @@
+import { lazy } from "react";
+const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
+
+var indexRoutes = [{ path: "/", name: "Dashboard", component: FullLayout }];
+
+export default indexRoutes;
